c5a462fc1145fda1fb3471258913769aac3096df
vivekascoder <vivekascoder@gmail.com>
2020-10-03 10:15:33 +0000

    Content can be seen from the homepage

--- app/models.py
+++ app/models.py
@@ -11,3 +11,5 @@ class Todo(models.Model):
     title = models.CharField(max_length=50)
     date = models.DateTimeField(auto_now_add=True)
     status = models.BooleanField(default=False)
+    def __str__(self):
+        return self.title
--- app/views.py
+++ app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, HttpResponse
 from django.views import View
-
+from app.models import Todo
 
 
 def home(request):
-    return HttpResponse("This is homepage.")
+    todos = Todo.objects.all()
+    data = {
+        'todos': todos,
+        'name': "Vivek"
+    }
+    return render(request, "index.html", context=data)
