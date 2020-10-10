fac104ab97a34c16ce419b8a88cc8b897173b718
vivekascoder <vivekascoder@gmail.com>
2020-10-10 19:09:17 +0000

    THIRD

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
+from django.urls import path, include
+from accounts import views as accounts_views
 
 urlpatterns = [
-    
+    path('', include("django.contrib.auth.urls")),
+    path('register', accounts_views.register_view, name="register")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+        else:
+            return redirect("register")
+    form = UserCreationForm()
+    return render(request, 'registration/register.html', context = {'form': form})
